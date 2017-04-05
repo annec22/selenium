47125442c2a8bbb372410adda78adf125b4ab5ed
carlou22 <annecalija@gmail.com>
2017-04-05 13:49:41 +0800

    Add browsers Firefox, Ie an Opera in open_close_browser.py

--- src/open_close_browser.py
+++ src/open_close_browser.py
@@ -8,3 +8,26 @@ print "Quitting Chrome browser.."
 driver_chrome.quit()
 print "Done."
 
+print "Opening Firefox..."
+driver_firefox = webdriver.Firefox()
+print "Visiting Tumblr.."
+driver_firefox.get("https://www.tumblr.com/")
+print "Quitting Firefox browser.."
+driver_firefox.quit()
+print "Done."
+
+print "Opening IE..."
+dirver_ie = webdriver.Ie()
+print "Visiting Tumblr.."
+dirver_ie.get("https://www.tumblr.com/")
+print "Quitting IE browser.."
+dirver_ie.quit()
+print "Done."
+
+print "Opening Opera..."
+driver_opera = webdriver.Opera()
+print "Visiting Tumblr.."
+driver_opera.get("https://www.tumblr.com/")
+print "Quitting Opera browser.."
+driver_opera.quit()
+print "Done."
